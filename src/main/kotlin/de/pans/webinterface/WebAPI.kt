7e4e4e4f36ddbef005a38a6e86a98c4e4eb9b89e
Jens Rosenbauer <jens.rosenbauer@t-online.de>
2021-11-21 18:41:02 +0100

    OKHTTP ist scheiße. Die threads sind kaputt

--- src/main/kotlin/de/pans/webinterface/WebAPI.kt
+++ src/main/kotlin/de/pans/webinterface/WebAPI.kt
@@ -1,14 +1,35 @@
 package de.pans.webinterface
 
-import de.pans.webinterface.executer.Button
 import org.json.JSONObject
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
-class WebAPI(){
+class WebAPI(
+    val url: String = "ws://localhost:80/",
+    val user: String = "remote",
+    val pwHash: String = "81dc9bdb52d04dc20036dbd8313ed055",
+) {
 
-    val sessionNr = 0
+    lateinit var websocketClient: Dot2WebsocketClient
+    var sessionNr = 0
+        private set
+
+    init {
+        val webSocketClient = Dot2WebsocketClient(url, user, pwHash)
+
+        when(webSocketClient.startClient()){
+            ServerResponse.OK ->            log("logged in successfully")
+            ServerResponse.TimeOut ->       log("Time out... failed to login")
+            ServerResponse.Unauthorized ->  log("Unauthorized... failed to login")
+            ServerResponse.Undefined ->     log("an internal error occurred... failed to login")
+            else ->                         log("something weired happened")
+        }
+
+        sessionNr = webSocketClient.sessionNr
+    }
 
 
-    fun readButtonState(ID: Int): Int{
+    fun readButtonState(ID: Int): Int {
 
         var startIndex = arrayOf<Int>()
         var itemsCount = arrayOf<Int>()
@@ -16,18 +37,17 @@ class WebAPI(){
         when (ID) {
             in 0..100 -> {
                 startIndex = arrayOf(ID, 100, 200)
-                itemsCount = arrayOf(1,0,0)
+                itemsCount = arrayOf(1, 0, 0)
             }
             in 101..200 -> {
                 startIndex = arrayOf(0, ID, 200)
-                itemsCount = arrayOf(0,1,0)
+                itemsCount = arrayOf(0, 1, 0)
             }
             in 201..300 -> {
                 startIndex = arrayOf(0, 100, ID)
-                itemsCount = arrayOf(0,0,1)
+                itemsCount = arrayOf(0, 0, 1)
             }
         }
-
 
 
         val objString = "{\"requestType\":\"playbacks\"," +
@@ -42,5 +62,8 @@ class WebAPI(){
         return 0
     }
 
+    private fun log(obj: Any?) {
+        println("${DateTimeFormatter.ofPattern("yyyy-MM-dd HH-mm-ss_SSS").format(LocalDateTime.now())}: ${obj.toString()}")
+    }
 
 }
--- src/main/kotlin/de/pans/webinterface/WebSocketListener.kt
+++ src/main/kotlin/de/pans/webinterface/WebSocketListener.kt
@@ -5,36 +5,64 @@ import kotlinx.coroutines.launch
 import okhttp3.*
 import okio.ByteString
 import org.json.JSONObject
+import java.lang.Exception
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.concurrent.SynchronousQueue
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 
 fun main(args: Array<String>) {
-    Dot2ClientWebSocket().startClient()
+//    Dot2WebsocketClient().startClient()
+    WebAPI()
 }
 
-class Dot2ClientWebSocket : WebSocketListener() {
+class Dot2WebsocketClient(
+    url: String = "ws://localhost:80/",
+    user: String = "remote",
+    password: String = "81dc9bdb52d04dc20036dbd8313ed055",
+) : WebSocketListener() {
 
-    private val NORMAL_CLOSURE_STATUS = 1000
+    //    private val NORMAL_CLOSURE_STATUS = 1000
     private var isRunning = false
 
-    private var sessionNr = 0
+    private val watingThread = Thread()
+
+    private var loginResponse = SynchronousQueue<ServerResponse>()
+
+
+    var sessionNr = 0
+        private set
     private var requestCounter = 0
     private var maxRequests = 0
 
 
-    fun startClient(){
+    fun startClient(): ServerResponse {
         val httpClient = OkHttpClient()
         val request = Request.Builder().url("ws://localhost:80/").build()
 
-        val listener = Dot2ClientWebSocket()
+        val listener = Dot2WebsocketClient()
         val webSocket = httpClient.newWebSocket(request, listener)
+        log("starting up Client")
 
+//        Thread.sleep(500)
+//        println("loginResponse: $loginResponse")
+
+
+//        waitUntilLoggedIn(5000)
+
+        var retVal = loginResponse.poll(5000, TimeUnit.MILLISECONDS)
+
+        if(retVal == null) retVal = ServerResponse.TimeOut
+
+        return retVal
     }
 
     override fun onOpen(webSocket: WebSocket, response: Response) {
         super.onOpen(webSocket, response)
         isRunning = true
-        println("onOpen")
+        log("onOpen")
         GlobalScope.launch {
             while (isRunning) {
                 webSocket.send(Random(0).nextInt().toString())
@@ -45,76 +73,112 @@ class Dot2ClientWebSocket : WebSocketListener() {
 
     override fun onMessage(webSocket: WebSocket, text: String) {
         super.onMessage(webSocket, text)
-        outputData("Receiving $text")
+
+        log("in  $text")
 
         val obj = JSONObject(text)
 
+        if (obj.has("status")) {
+            when (obj.getString("status")) {
+                "server ready" -> sendMessage(webSocket, "{\"session\":$sessionNr}")
+            }
+        }
+
         if (obj.has("session")) {
             sessionNr = obj.getInt("session")
-            println("updated sessionNr: $sessionNr")
+            log("in  sessionNr=$sessionNr")
         }
 
-        if (obj.has("status") && obj.getString("status") == "server ready") {
-            sendMessage(webSocket, "{\"session\":$sessionNr}")
-
-//            sendMessage(webSocket,"{\"requestType\":\"getdata\",\"data\":\"set,clear,high\",\"session\":$sessionNr,\"maxRequests\":1}")
-            println("send session = 0")
+        if (obj.has("forceLogin")) {
+            when (obj.getBoolean("forceLogin")) {
+                true -> {
+                    sendMessage(webSocket,
+                        "{\"requestType\":\"login\",\"username\":\"remote\",\"password\":\"81dc9bdb52d04dc20036dbd8313ed055\",\"session\":$sessionNr,\"maxRequests\":10}\'")
+                }
+            }
         }
 
-        if (obj.has("responseType") && obj.has("result") && obj.getString("responseType") == "login" && obj.getBoolean("result")) {
-            println("logged in successfully")
-            runtime(webSocket)
+        if (obj.has("responseType") && obj.getString("responseType") == "login") {
+            if (obj.has("result") && obj.getBoolean("result")) {
+                log("logged in successfully------")
+
+
+                println("loginResponse: putting OK $loginResponse")
+                try {
+                    loginResponse.put(ServerResponse.OK)
+                } catch (e: Exception) {
+                    println(e)
+                }
+                watingThread.stop()
+
+//                runtime(webSocket)
+
+            } else {
+                log("failed to log in")
+                try {
+                    loginResponse.put(ServerResponse.Unauthorized)
+                } catch (e: Exception) {
+                    println(e)
+                }
+                watingThread.stop()
+
+            }
         }
+}
 
+override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+    super.onMessage(webSocket, bytes)
+    log("in:  bytes: ${bytes.hex()}")
+}
 
-        if (obj.has("forceLogin") && obj.getBoolean("forceLogin")) {
-            println("send login")
-            sessionNr = obj.getInt("session")
-            sendMessage(webSocket,
-                "{\"requestType\":\"login\",\"username\":\"remote\",\"password\":\"81dc9bdb52d04dc20036dbd8313ed055\",\"session\":$sessionNr,\"maxRequests\":10}\'")
+override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+    super.onClosing(webSocket, code, reason)
+    log("closing: $code $reason")
+    isRunning = false
+    log("webSocket closed")
+}
 
-
-        }
-
-
+fun runtime(webSocket: WebSocket) {
+    GlobalScope.launch {
+        sendMessage(webSocket,
+            "{\"requestType\":\"playbacks\",\"startIndex\":[1,105,200],\"itemsCount\":[0,1,0],\"pageIndex\":3,\"itemsType\":[2,3,3],\"view\":2,\"execButtonViewMode\":1,\"buttonsViewMode\":0,\"session\":$sessionNr,\"maxRequests\":1}"
+        )
     }
+}
 
-    override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
-        super.onMessage(webSocket, bytes)
-        outputData("Receiving bytes : " + bytes.hex())
+
+fun sendMessage(webSocket: WebSocket, msg: String) {
+    GlobalScope.launch {
+        webSocket.send(msg)
+        log("out $msg")
     }
+}
 
-    override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
-        super.onClosing(webSocket, code, reason)
-        outputData("$code $reason")
-        isRunning = false
-        println("log: webSocket closed ${System.currentTimeMillis()}")
-    }
+private fun log(obj: Any?) {
+    println("${
+        DateTimeFormatter.ofPattern("yyyy-MM-dd HH-mm-ss_SSS").format(LocalDateTime.now())
+    }: ${obj.toString()}")
+}
 
-    private fun outputData(outputString: String) {
-        println("web socket$outputString")
-    }
+private fun waitUntilLoggedIn(timeout: Long) {
+//        val endTime = System.currentTimeMillis() + timeout
 
-
-    fun runtime(webSocket: WebSocket) {
-
-
-        println("runtime")
-
-        GlobalScope.launch {
-            sendMessage(webSocket,
-                "{\"requestType\":\"playbacks\",\"startIndex\":[1,105,200],\"itemsCount\":[0,1,0],\"pageIndex\":3,\"itemsType\":[2,3,3],\"view\":2,\"execButtonViewMode\":1,\"buttonsViewMode\":0,\"session\":$sessionNr,\"maxRequests\":1}"
-            )
-        }
-
-
-    }
-
-
-    fun sendMessage(webSocket: WebSocket, msg: String) {
-        GlobalScope.launch {
-            println("sending Message: $msg")
-            webSocket.send(msg)
+    watingThread.run {
+        Thread.sleep(timeout)
+        println("loginResponse: putting TimeOut $loginResponse")
+        try {
+            loginResponse.add(ServerResponse.TimeOut)
+        } catch (e: Exception) {
+            println(e)
         }
     }
+    return
+}
+}
+
+enum class ServerResponse(i: Int) {
+    Undefined(-1),
+    OK(200),
+    Unauthorized(401),
+    TimeOut(408)
 }
